22ee05cdcf976c4bd10becde0de895b284d072cb
Santiago Yunda <desarrollador095@expansionti.com>
2019-01-17 10:20:58 -0500

    update settings

--- expancore/settings.py
+++ expancore/settings.py
@@ -2,6 +2,7 @@ from datetime import timedelta
 import os
 import mimetypes
 import django_heroku
+import rest_framework_jwt
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
